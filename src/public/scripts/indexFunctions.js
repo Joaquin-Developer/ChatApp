1c975a12c5f6157bba5db7e2ccff8d0864090f2f
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2020-12-27 23:42:55 -0300

    Avanzo con frontend de la aplicación

--- src/public/scripts/indexFunctions.js
+++ src/public/scripts/indexFunctions.js
@@ -1,4 +1,45 @@
+/**
+ * index.html functions
+ */
+
+const txtMensaje = document.getElementById("txtMensaje");
+const messagesList = document.getElementById("messagesList");
 
 addEventListener("load", function() {
-    console.log("Hola");
+    // console.log("Hola");
+    // hacer algunas cositas mas aquí ...
 });
+
+document.getElementById("acercaDe").addEventListener("click", function() {
+    alert("Aun no programé esta sección...");
+});
+
+document.getElementById("btnSend").addEventListener("click", sendMessage);
+
+txtMensaje.addEventListener("keypress", function(event) {
+    // usuario presiona enter
+	if (event.keyCode === 13) sendMessage(); 
+});
+
+function sendMessage() {
+    const message = txtMensaje.value;
+
+	if (message.length === 0) {
+		alert("Debe ingresar un mensaje");
+	} else {
+		// const msgElement = document.createElement("P");
+		// msgElement.appendChild(document.createTextNode(`${sessionStorage.getItem("userName")}: ${message}`));
+        // document.getElementById('messages').appendChild(msgElement);
+
+        messagesList.value += (message + "\n");
+        // ajusto scroll al final:
+        messagesList.scrollTop = messagesList.scrollHeight;
+		cleanTextBox();
+	}
+
+}
+
+function cleanTextBox() {	
+	txtMensaje.value = "";
+}
+
